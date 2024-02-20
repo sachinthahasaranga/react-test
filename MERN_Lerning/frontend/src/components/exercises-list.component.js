79d337574b5bbb72b18647cb816897f66f01ec2b
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2024-02-20 14:33:55 +0530

    front end

--- MERN_Lerning/frontend/src/components/exercises-list.component.js
+++ MERN_Lerning/frontend/src/components/exercises-list.component.js
@@ -35,10 +35,24 @@ export default class ExercisesList extends Component {
     
     
     
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <p>this is Exercise list</p>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.exerciseList()}
+                    </tbody>
+                </table>
             </div>
         )
     }
