23c449cff131dd60d17edf986155d049676c4d6c
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2024-02-20 14:43:40 +0530

    test

--- MERN_Lerning/frontend/src/components/exercises-list.component.js
+++ MERN_Lerning/frontend/src/components/exercises-list.component.js
@@ -1,40 +1,66 @@
 import { Component } from "react";
 import axios from 'axios';
 
+const Exercise = (props) => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0, 10)}</td>
+        <td>
+            <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+            <a
+                href="#"
+                onClick={() => {
+                    props.deleteExercise(props.exercise._id);
+                }}
+            >
+                delete
+            </a>
+        </td>
+    </tr>
+);
+
+
+
 export default class ExercisesList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.deleteExercise = this.deleteExercise.bind(this);
 
-        this.state = {exercises : []};
+        this.state = { exercises: [] };
 
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
         axios.get('http://localhost:5000/exercises/')
             .then(response => {
-                this.setState({exercises : response.data})
+                this.setState({ exercises: response.data })
             })
-            .catch((error)=>{
+            .catch((error) => {
                 console.log(error);
             })
     }
-    
-    deleteExercise(id){
-        axios.delete('http://localhost:5000/exercises/'+id)
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/' + id)
             .then(res => console.log(res.data));
 
         this.setState({
             exercises: this.state.exercises.filter(el => el._id !== id)
         })
     }
-    
-    
-    
-    
-    
-    
+
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
+        })
+    }
+
+
+
+
     render() {
         return (
             <div>
